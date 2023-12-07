6a665a3c105af1d7a17a61792740d8a7aa218834
MariaClaraValotti <email do usurio>
2023-12-07 19:41:39 -0300

    Logica

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("ano novo esta chegando")
+let nome = "Mariah Carey"
+let idade = 54
+let ano = 2023
+console.log(ano - idade)
+let nascimento = 27
+console.log(nascimento)
+
+let quartaFeira = true
+let quartafeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
+let show = "roberto carlos "
+if (show >= 18) { console.log(`Você é maior de idade, poderá curtir o show do ${show} com participação especial de ${nome}.`) }
+else { console.log("Sinto muito, mas assistirá da tv globo") }
+
+let mês = "Agosto"
+let mêsDoniver = "Junho"
+let mêsDezembro = "Dezembro"
+if (mês =="Agosto" || mêsDonniver == "Junho" || mêsDezembro != "janeiro") {
+    console.log("Uma ou mais opções estão corretas")
+}
+else { console.log("O mês escolhido foi o mês junho.") }
+
+
